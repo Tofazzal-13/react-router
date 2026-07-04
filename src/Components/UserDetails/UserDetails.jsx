34013dbf49434794fcf74805cffae97551022aac
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-07-04 22:27:03 +0600

    dynamic route

--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UserDetails = () => {
+    const user = useLoaderData();
+    const {website, name} = user;
+    
     return (
         <div>
             <h3>users details</h3>
+            <p>Name: {name} </p>
+            <p>Website: {website} </p>
         </div>
     );
 };
