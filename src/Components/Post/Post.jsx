ed8d4d0ed14c8d81f7a4bca79ea18687703e14d2
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-07-06 22:19:44 +0600

    learn usenavigate

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
-const Post = ({post}) => {
-    const {id, title, body} = post;
+const Post = ({ post }) => {
+    const { id, title, body } = post;
+    const navigate = useNavigate()
+    const handleNavigate = () => {
+        navigate(`/posts/${id}`)
+    }
     return (
-        <div style={{border:"2px solid yellow", padding:"10px", margin:"10px", borderRadius:"10px"}}>
+        <div style={{ border: "2px solid yellow", padding: "10px", margin: "10px", borderRadius: "10px" }}>
             <h3>Title: {title} </h3>
             <p>{body} </p>
-            <Link to={`/posts/${id}`}>Show Details</Link>
+            <Link to={`/posts/${id}`}>
+                <button>Show Details</button>
+            </Link>
+            <button onClick={handleNavigate}>Details of: {id} </button>
         </div>
     );
 };
