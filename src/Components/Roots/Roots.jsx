528f2418ee0781617c911fec15d6dd4eb13acc94
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-07-06 23:25:37 +0600

    Explore hooks useNavigate and useNavigator for router spinner

--- src/Components/Roots/Roots.jsx
+++ src/Components/Roots/Roots.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 import Sidebar from '../Sidebar/Sidebar';
 import "./Root.css"
 
 const Roots = () => {
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location)
     return (
         <div>
             <Header></Header>
             <div className='root-main'>
                 <Sidebar ></Sidebar>
+                { isNavigating  && <span>loading..</span> }
                 <Outlet ></Outlet>
             </div>
             <Footer></Footer>
