2a71175ffd00de2742997b65276975b61aa8d361
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-07-07 09:38:22 +0600

    Navigate useLocation

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,17 +1,24 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useLocation } from 'react-router';
 import UsersDetails2 from '../UsersDetails2/UsersDetails2';
 
 const User = ({user}) => {
     const {id,name, email, phone} = user ;
     const [showinfo, setShowinfo] = useState(false)
+    const [visitHome, setVisitHome] = useState(false)
     const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json())
+    const location = useLocation();
+    console.log(location);
+    
 
     const userStyle = {
         border: "2px solid yellow",
         borderRadius: "20px",
         padding: "10px",
         margin: "10px"
+    }
+    if(visitHome){
+        return <Navigate to={"/"}></Navigate>
     }
     return (
         <div style={userStyle}>
@@ -25,6 +32,7 @@ const User = ({user}) => {
                     <UsersDetails2 userPromise= {userPromise}></UsersDetails2>
                 </Suspense>
             }
+            <button onClick={() => setVisitHome(true)}>Visit Home</button>
         </div>
     );
 };
